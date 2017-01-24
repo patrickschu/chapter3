c608dd4f495da5833e335b7afe7bfd9e86ca6213
patrick <patrickschultz@utexas.edu>
2017-01-24 12:32:48 -0600

    add barplot machinery

---
+++ exploring_control_stimulus_0124.R
@@ -0,0 +1,38 @@
+
+"""
+Computations with the first run on MechTurk
+"""
+
+
+csvcleaner=function(spread_sheet) {
+	#takes out all the stuff from MechTurk and Qualtrics we don't want
+	badcols= c("IPAddress", "ResponseId", "mTurkCode")
+	spread_sheet= spread_sheet[,!(names(spread_sheet) %in% badcols)]
+	return(spread_sheet)
+}
+
+
+barplot_by_column= function(vector_of_column_indexes){
+	#takes a list of column indexes, plots bars for each
+	for (ind in vector_of_column_indexes)
+		{
+		outputfile= paste(ind, "barplot.png", sep+"_")
+		print (ind)
+		png(outputfile,  width=960, height=640, res=100)
+		barplot (xtabs(~spread[[ind]]))
+		dev.off()
+		}	
+		
+}
+
+spread=read.csv("/Users/ps22344/Downloads/chapter3/csv/creating_stimulus_0123_January 24, 2017_11.25.csv", header=T)
+
+print (colnames(spread))
+
+spread=csvcleaner(spread)
+
+print (colnames(spread))
+
+barplot_by_column(c(17:24))
+
+
