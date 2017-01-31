21cb72b895aa77519152d58621fc645205ece973
patrick <patrickschultz@utexas.edu>
2017-01-31 12:44:10 -0600

    clean preview stuff

--- exploring_control_stimulus_0124.R
+++ exploring_control_stimulus_0124.R
@@ -9,6 +9,8 @@ csvcleaner=function(spread_sheet) {
 	spread_sheet=spread_sheet[2:nrow(spread_sheet),]
 	badcols= c("IPAddress", "ResponseId", "mTurkCode")
 	spread_sheet= spread_sheet[,!(names(spread_sheet) %in% badcols)]
+	#previews is when we go in and mess wit it
+	spread_sheet= spread_sheet[!(spread_sheet[['Distribution.Channel']]=='preview'), ]
 	#from 
 	spread_sheet= droplevels(spread_sheet)
 
@@ -31,9 +33,10 @@ barplot_by_column= function(spread_sheet, vector_of_column_indexes){
 }
 
 #read in & clean
-spread=read.csv("~/Downloads/chapter3/creating_stimulus_0123_3rddrun - Copy_January 29, 2017_10.51.csv", skip=1, header=T)
+spread=read.csv("~/Downloads/creating_stimulus_0123_3rddrun - Copy_January 31, 2017_12.24.csv",  skip=1, header=T)
+spread[2,]
 spread=csvcleaner(spread)
-
+summary(spread)
 #plot
 barplot_by_column(spread, c(17:24))
 
