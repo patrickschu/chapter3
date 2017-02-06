560587c992e766c6fd8d99faa634938115c03834
patrick <patrickschultz@utexas.edu>
2017-02-06 12:42:29 -0600

    add chi square functionality

--- perception_0206.R
+++ perception_0206.R
@@ -23,3 +23,49 @@ print (summary(spread))
 perceptionfeatures=c()
 participantfeatures=c()
 
+##
+#Chi square comparison
+##
+
+
+
+
+# 1 Start Date
+# 2 End Date
+# 3 Response Type
+# 4 IP Address
+# 5 Progress
+# 6 Duration (in seconds)
+# 7 Finished
+# 8 Recorded Date
+# 9 Response ID
+# 10 Recipient Last Name
+# 11 Recipient First Name
+# 12 Recipient Email
+# 13 External Data Reference
+# 14 Location Latitude
+# 15 Location Longitude
+# 16 Distribution Channel
+# 17 The author is ...
+# 18 The author is ....1
+# 19 The author is ....2
+# 20 The author is writing for a ...
+# 21 The author seems ...
+# 22 I'd guess the author is ...
+# 23 The author seems ....1
+# 24 The author seems ....2
+# 25 The author seems ....3
+# 26 Men are ...
+# 27 Women are ...
+# 28 Would you reply to this ad?
+# 29 Do you have any further comments? Please share!
+# 30 I am ... - Selected Choice
+# 31 I am ... - other, please specify - Text
+# 32 I consider myself ... - Selected Choice
+# 33 I consider myself ... - other, please specify - Text
+# 34 My age:
+# 35 I consider myself ... - Selected Choice.1
+# 36 I consider myself ... - other, please specify - Text.1
+# 37 I grew up in this city and this state:
+# 38 I now live in this city:
+# 39 mTurkCode
