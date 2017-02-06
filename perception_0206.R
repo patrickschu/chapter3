ee309fd7e561f86d199dc197b03271338792ec0a
patrick <patrickschultz@utexas.edu>
2017-02-06 15:31:08 -0600

    add chisq function

--- perception_0206.R
+++ perception_0206.R
@@ -5,6 +5,11 @@
 
 #moving parts
 #setwd('/Users/ps22344/Downloads/chapter3/rplots')
+
+
+
+setwd('~/Downloads/chapter3')
+install('surveytools')
 setwd('~/Downloads/chapter3/rplots')
 filename="/Users/ps22344/Desktop/emoticons_0203_February 6, 2017_12.14.csv"
 
@@ -20,52 +25,60 @@ print (summary(spread))
 
 #output and inspect
 #barplot_by_column(spread, "emoticons", c(17:34))
-perceptionfeatures=c()
-participantfeatures=c()
+
+perceptionfeatures=c(            
+"author_gender"                                     
+, "author_orient"                                   
+, "author_friendly"                                   
+, "author_audience"                       
+, "author_sensitive"                                  
+, "author_ethnicity"                           
+, "author_assertive"                                
+, "author_attractive"                                
+, "author_education"                                                                    
+, "would_you_reply"                           
+)
+
+perceptionfeatures_text=c(
+"men_are"                                           
+, "women_are"   
+, "further_comments"
+)
+
+participantfeatures=c(
+"participant_gender"                            
+, "participant_gender_other"               
+, "participant_ethnicity"               
+, "participant_ethnicity_other"  
+, "participant_age"                                               
+, "participant_orientation"             
+, "participant_orientation_other"
+, "participant_grew_up"                
+, "participant_residence") 
+
+
 
 ##
 #Chi square comparison
 ##
+#compute author features
 
+chisquaretester= function(control_stimulus, stimulus, column)
+{
+	#takes the spreadsheet for contronl group (control_stimulus), treatment group to be compared to control_stimulus
+	#takes vector_of_columns with names, indices to computed. They need to be equivalent in both spreadsheets
+	#make tables control stimulus and stimulus
+	control= na.omit(control_stimulus[[column]])
+	treatment= na.omit(stimulus[[column]])
+	print ("checking for NAs")
+	cat (length(control_stimulus[[column]])- length(control), " NAs found in 'control_stimulus\n")
+	cat (length(stimulus[[column]])-length(treatment), " NAs found in 'stimulus'")
+	
+	
+	#print (summary(control))
+	#print (summary(treatment))
+	
+}
 
+chisquaretester(spread, spread, 16)
 
-
-# 1 Start Date
-# 2 End Date
-# 3 Response Type
-# 4 IP Address
-# 5 Progress
-# 6 Duration (in seconds)
-# 7 Finished
-# 8 Recorded Date
-# 9 Response ID
-# 10 Recipient Last Name
-# 11 Recipient First Name
-# 12 Recipient Email
-# 13 External Data Reference
-# 14 Location Latitude
-# 15 Location Longitude
-# 16 Distribution Channel
-# 17 The author is ...
-# 18 The author is ....1
-# 19 The author is ....2
-# 20 The author is writing for a ...
-# 21 The author seems ...
-# 22 I'd guess the author is ...
-# 23 The author seems ....1
-# 24 The author seems ....2
-# 25 The author seems ....3
-# 26 Men are ...
-# 27 Women are ...
-# 28 Would you reply to this ad?
-# 29 Do you have any further comments? Please share!
-# 30 I am ... - Selected Choice
-# 31 I am ... - other, please specify - Text
-# 32 I consider myself ... - Selected Choice
-# 33 I consider myself ... - other, please specify - Text
-# 34 My age:
-# 35 I consider myself ... - Selected Choice.1
-# 36 I consider myself ... - other, please specify - Text.1
-# 37 I grew up in this city and this state:
-# 38 I now live in this city:
-# 39 mTurkCode
