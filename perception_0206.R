9b3ed51bc6f6f7d72f683e44958f0e3857667b3f
patrick <patrickschultz@utexas.edu>
2017-02-07 14:26:59 -0600

    add basicstatsmaker to output dataset stats

--- perception_0206.R
+++ perception_0206.R
@@ -1,41 +1,7 @@
 ###
 #Computations with perception surveys on MechTurk
 ###
-
 library(devtools)
-
-#moving parts
-setwd('~/Downloads/chapter3')
-install('surveytools')
-library('surveytools')
-
-#setwd('/Users/ps22344/Downloads/chapter3/rplots')
-controlspread= read.csv("/Users/ps22344/Downloads/control_stimulus_adapted_0206.csv",  header=T, na.strings=c(""))
-controlspread=csvcleaner(controlspread)
-print (summary(controlspread))
-
-
-
-setwd('~/Downloads/chapter3/rplots')
-filename="/Users/ps22344/Desktop/emoticons_0203_February 6, 2017_12.14.csv"
-
-#Read in, clean
-spread=read.csv(filename,   header=T, na.strings=c(""))
-#spread=read.csv(filename,  header=T)
-cat ("Input file has ", nrow(spread), "rows")
-spread=csvcleaner(spread)
-cat ("Cleaned file has ", nrow(spread), "rows")
-print (summary(spread))
-
-
-
-
-
-
-
-#output and inspect
-#barplot_by_column(spread, "emoticons", c(17:34))
-
 perceptionfeatures=c(            
 "author_gender"                                     
 , "author_orient"                                   
@@ -68,25 +34,63 @@ participantfeatures=c(
 
 
 
+#moving parts
+setwd('~/Downloads/chapter3')
+install('surveytools')
+library('surveytools')
+
+#setwd('/Users/ps22344/Downloads/chapter3/rplots')
+controlspread= read.csv("/Users/ps22344/Downloads/control_stimulus_adapted_0206.csv",  header=T, na.strings=c(""))
+controlspread=csvcleaner(controlspread)
+print (summary(controlspread))
+
+
+
+setwd('~/Downloads/chapter3/rplots')
+filename="/Users/ps22344/Downloads/emoticons_0207.csv"
+
+#Read in, clean
+spread=read.csv(filename,   header=T, na.strings=c(""))
+#spread=read.csv(filename,  header=T)
+cat ("Input file has ", nrow(spread), "rows")
+spread=csvcleaner(spread)
+cat ("Cleaned file has ", nrow(spread), "rows")
+print (summary(spread))
+
+
+#output and inspect
+barplot_by_column(spread, "emoticons", perceptionfeatures)
+
+
+
+
 ##
 #Chi square comparison
 ##
 #compute author features
 
-chisquaretester= function(control_stimulus, stimulus, column)
+chisquaretester= function(control_stimulus, stimulus, column, output="csv")
 {
 	#takes the spreadsheet for contronl group (control_stimulus), treatment group to be compared to control_stimulus
 	#takes vector_of_columns with names, indices to computed. They need to be equivalent in both spreadsheets
 	#make tables control stimulus and stimulus
 	control= na.omit(control_stimulus[[column]])
 	treatment= na.omit(stimulus[[column]])
-	print ("checking for NAs")
-	cat (length(control_stimulus[[column]])- length(control), " NAs found in 'control_stimulus\n")
-	cat (length(stimulus[[column]])-length(treatment), " NAs found in 'stimulus'")
-	cat ("\nInvestigating feature", colnames(control_stimulus)[column], colnames(stimulus)[column], "\n")
-	print (table(control))
-	print (table(treatment))
 	chisquare= chisq.test(rbind(table(control), table(treatment)))
+	if (output=="text")
+	{
+		print ("checking for NAs")
+		cat (length(control_stimulus[[column]])- length(control), " NAs found in 'control_stimulus\n")
+		cat (length(stimulus[[column]])-length(treatment), " NAs found in 'stimulus'")
+		cat ("\nInvestigating feature", column, "\n")
+		print (table(control))
+		print (table(treatment))
+	}
+	else (output=="csv")
+	{
+		write.csv(sapply(chisquare, unlist))
+	}
+	
 	cat ("\np value", chisquare$p.value, "\n")
 	cat ("expected: ", chisquare$expected, "\nobserved: ", chisquare$observed, "\n")
 	
@@ -94,5 +98,38 @@ chisquaretester= function(control_stimulus, stimulus, column)
 
 for (c in perceptionfeatures) {cat("\n++++\n"); chisquaretester(controlspread, spread, c)}
 
-chisquaretester(controlspread, spread, 17)
+chisquaretester(controlspread, spread, 17, output="csv")
+
+#' Print basic stats
+#' 
+#' Takes a spread_sheet, prints relevant stats for qualtrics survey
+#'@param
+#' spread_sheet Our Qualtrics spreadsheet
+#' column_indexes A vector of column names or numbers, columns must be factors
+#'@keywords why not
+basicstatsmaker= function(spread_sheet, column_indexes)
+{
+	#print out: number of total responses number of men, women
+	cat ("Total number of responses in spread_sheet", nrow(spread_sheet), "\n")
+	for (ind in column_indexes)
+	{
+		if (is.factor(spread_sheet[[ind]]))
+			{
+			cat("\n\n+++", ind, ", ", nrow(spread_sheet) - length(na.omit(spread_sheet[[ind]])), "NAs in here" )
+			sapply(levels(spread_sheet[[ind]]), function(x) cat("\n", x, ",", nrow(spread_sheet[spread_sheet[[ind]]==x,]), ",", nrow(spread_sheet[spread_sheet[[ind]]==x,])/nrow(spread_sheet)))
+			}
+		if (is.numeric(spread_sheet[[ind]]))
+			{
+			vec= na.omit(spread_sheet[[ind]])
+			cat("\n\n+++", ind )
+			cat("\nmean", mean(vec), 
+			"\nmedian,", median(vec), 
+			"\nstandard_dev,", sd(vec), 
+			"\nmin-max,", min(vec),"-", max(vec))
+			}
+	}
+	
+	
+}
+basicstatsmaker(controlspread, participantfeatures[c(1:(length(participantfeatures)-2))])
 
