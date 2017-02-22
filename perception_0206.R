198be98ed5b5b82cfd67079d65b59732230175a4
patrick <patrickschultz@utexas.edu>
2017-02-21 18:30:36 -0600

    move relativeplotter

--- perception_0206.R
+++ perception_0206.R
@@ -24,7 +24,7 @@ plotnames=c(
 
 
 files=c(
-'/Users/ps22344/Downloads/adapted_control_0206.csv', 
+#'/Users/ps22344/Downloads/adapted_control_0206.csv', 
 '/Users/ps22344/Downloads/Capitals_0212.csv', 
 '/Users/ps22344/Downloads/Clippings_0208.csv', 
 '/Users/ps22344/Downloads/Prosody_0211.csv', 
@@ -93,7 +93,7 @@ setwd('~/Downloads/chapter3/rplots')
 ###
 #
 ###
-filename='/Users/ps22344/Downloads/Single\ letter_0218.csv'
+filename='/Users/ps22344/Downloads/Punctuation_0208.csv'
 ###
 #
 ###
@@ -115,7 +115,7 @@ fullspread=surveytools:::spreadsheetbuilder(files)
 #output and inspect
 #surveytools:::barplot_by_column(spread, "capitals", perceptionfeatures)
 
-for (c in perceptionfeatures) {cat("\n++++\n"); surveytools:::chisquaretester2(controlspread, spread, c, output="csv")}
+for (c in participantfeaturesnumeric) {cat("\n++++\n"); surveytools:::chisquaretester2(controlspread, spread, c, output="text")}
 
 # surveytools:::basicstatsmaker(controlspread, participantfeatures[c(1:(length(participantfeatures)-2))])
 surveytools:::basicstatsmaker(spread, perceptionfeatures)
@@ -148,78 +148,6 @@ surveytools:::basicstatsmaker(spread, perceptionfeatures)
 # }
 
 
-##
-#PLOTTING RELATIVE TO CONTROL
-##
-#plot the means of control
-
-relativeplotter= function(control_stimulus, data_set, vector_of_columns, filename)
-#plot the means for all stimuli to compare
-{
-	cat("running relativeplotter")
-	#these are our 0s
-	controlmeans= sapply(vector_of_columns, function(x) mean(as.numeric(control_stimulus[[x]]), na.rm=TRUE))
-	#out units
-	controlsd= sapply(vector_of_columns, function(x) sd(as.numeric(control_stimulus[[x]]), na.rm=TRUE))
-	#set up plot
-	#png(paste(filename,".png"), width=331.8, height=215.9, unit="mm", res=750)
-	plot(100, 
-	xlim=c(1,length(vector_of_columns)), 
-	ylim=c(.75,-.75),
-	xlab= "Characteristic",
-	ylab= "Distance to control mean (standard deviations)",
-	type="n", 
-	xaxt="n")
-	abline(a=0, b=0)
-	abline(v=2.5, lty=3)
-	axis(side=1, at=seq(1, length(vector_of_columns), 1), labels=plotnames, cex.axis=0.8)
-
-	#iterate over stimuli
-	stimuluscounter=0
-	for (lev in levels(data_set[['stimulus']]))
-		{
-		cat ("\n---\nStimulus is", lev)
-		stimuluscounter= stimuluscounter + 1	
-		colcounter= 1
-		for (column in vector_of_columns)
-			{
-			cat ("\n--working on", column, "mean:")
-			print (mean(as.numeric(na.omit(data_set[data_set[['stimulus']]==lev,][[column]]))))
-			cat ("controls", controlmeans[column], controlsd[column])
-			colmean= mean(as.numeric(na.omit(data_set[data_set[['stimulus']]==lev,][[column]])))
-			distance= (colmean - controlmeans[column]) / controlsd[column]
-			cat ("\ndistance:", distance)
-			#points(colcounter, distance, col=stimuluscounter)
-			text(colcounter, distance, col=stimuluscounter, labels= lev, cex=0.7)
-			colcounter= colcounter+1
-			}
-		}
-	
-	#add transparent text for orientation
-	text(1.5,-0.5, 
-	"f", 	
-	cex=4, 	
-	col=rgb(col2rgb("blue")['red',], col2rgb("blue")['green',], col2rgb("blue")['blue',], alpha=100, maxColorValue=255))
-	
-	
-	text(1.5,0.5, 
-	"m", 	
-	cex=4, 	
-	col=rgb(col2rgb("blue")['red',], col2rgb("blue")['green',], col2rgb("blue")['blue',], alpha=100, maxColorValue=255))
-	
-	
-	text(5/2+2.5,0.5, 
-	"less", 	
-	cex=4, 	
-	col=rgb(col2rgb("blue")['red',], col2rgb("blue")['green',], col2rgb("blue")['blue',], alpha=100, maxColorValue=255))
-	
-	text(5/2+2.5,-0.5, 
-	"more", 
-	cex=4, 	
-	col=rgb(col2rgb("blue")['red',], col2rgb("blue")['green',], col2rgb("blue")['blue',], alpha=100, maxColorValue=255))
-	
-	#dev.off()	
-}
 
 
 controlspread['stimulus']= as.factor('control')
