eca7119ee928c8c4889f8abf92e746029248c169
patrick <patrickschultz@utexas.edu>
2017-03-20 08:09:31 -0500

    these we use for paper; moved from perception_0206'

---
+++ rscripts/barplots_0320.R
@@ -0,0 +1,71 @@
+for (package in c("devtools", "roxygen2", "pwr", "effsize", "likert")) {
+    if (!require(package, character.only=T, quietly=T)) {
+        install.packages(package, repos='http://cran.us.r-project.org')
+        library(package, character.only=T)
+    }
+}
+
+setwd('~/Downloads/chapter3/surveytools')
+document()
+
+setwd('~/Downloads/chapter3')
+install('surveytools')
+library('surveytools')
+
+
+#BARPLOTTING
+barspread=surveytools:::spreadsheetbuilder(c('/Users/ps22344/Downloads/Clippings_0208.csv', '/Users/ps22344/Downloads/Prosody_0211.csv', '/Users/ps22344/Downloads/adapted_control_0206.csv'))
+###
+#
+###
+
+#gender by stimulus
+#ok so the consulting guy says my functions are overkill
+# i disagree, look at the mess below
+#this is terrible!
+png("barplottest.png", width=331.8, height=215.9, unit="mm", res=500)
+par(cex=1.25)
+percentages=aggregate(barspread[['author_gender']], list(barspread[['stimulus']]), function(x) table(x)/length(x))
+
+g=t(percentages[['x']])
+
+colnames(g)= c('Clippings', 'Prosody', 'Control')
+g= g[,c('Control', 'Clippings', 'Prosody')]
+
+par(mar=c(5.1,  4.1,  4.1, 12.0))
+barplot(g, 
+main= "Author gender by stimulus",
+#cex.main=1.5,
+col=c("black", "white"),
+ylab= "Proportion",
+legend.text= TRUE,
+args.legend= list(x=4.5,y=.9)
+)
+dev.off()
+
+
+#education by stimulus
+barspread=surveytools:::spreadsheetbuilder(c('/Users/ps22344/Downloads/Single\ letter_0218.csv','/Users/ps22344/Downloads/Clippings_0208.csv', '/Users/ps22344/Downloads/Prosody_0211.csv', '/Users/ps22344/Downloads/adapted_control_0206.csv'))
+
+
+levels(barspread$author_education) = list("Below average"= c("Somewhat uneducated",  "Very uneducated"),  "Average"= "Of average education", "Above average"=c("Very educated", "Somewhat educated"))
+percentages=aggregate(barspread[['author_education']], list(barspread[['stimulus']]), function(x) table(x)/length(x))
+f=t(percentages[['x']])
+colnames(f)= c('Single letter', 'Clippings', 'Prosody', 'Control')
+f= f[,c('Control', 'Clippings', 'Prosody', 'Single letter')]
+png("plot_education.png", width=331.8, height=215.9, unit="mm", res=500)
+par(cex=1.25)
+# #$mar
+# #[1]  5.1  4.1  4.1 12.0
+par(mar=c(5.1,  4.1,  4.1, 14.0))
+barplot(f,
+main= "Perceived author education",
+col= c(1,1,0),
+density= c(200,50,100),
+ylab= "Proportion",
+xlab= "Feature",
+legend.text= TRUE,
+args.legend= list(x=6.25,y=.9)
+)
+mtext(text="By stimulus, p < 0.05 only", side=3, line= 0.25)
+dev.off()
